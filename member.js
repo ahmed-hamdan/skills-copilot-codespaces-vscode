625c72a5847a2a624bf93273d3ce8d99f9844cfe
ahmed-hamdan <57875245+ahmed-hamdan@users.noreply.github.com>
2023-07-22 18:58:58 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,5 @@
 function skillMember(){
     return {
-        restrict: 'E',
-        templateUrl: 'app/components/skill/member.html',
-        controller: 'skillMemberController', 
-        controllerAs: 'skillMemberCtrl',
-        bindToController: true,
-        scope: {
-            member: '='
-        }
+        restrict: 'E'
     };
 }
